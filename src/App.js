7d0040ef44f72eac5ee0d278d6f575cf9a114ae9
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2024-04-11 13:46:48 +0530

    App reseted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import CountDown from './Components/CountDown';
 
 function App() {
   return (
     <div className="App">
-     
+     <CountDown/>
     </div>
   );
 }
---
+++ src/Components/CountDown.jsx
@@ -0,0 +1,5 @@
+import React,{useEffect,useState} from "react";
+export default function CountDown(){
+    
+    return <div className="countdown">hi</div>
+}
